3877b90fdd6eabbd24c6d39255763f0afa40f369
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-02-26 19:01:14 +0300

    Add Product component with styled elements

---
+++ Product.jsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+const ProductCard = styled.div`
+  width: 180px;
+  border-radius: 1rem;
+  border: 1px solid black;
+  margin-bottom: 2rem;
+  font-size: 10px;
+`;
+const Category = styled.h2`
+font-size: 1.2rem;
+  padding: 0.5rem;
+  background-color: rgb(185, 208, 250);
+  margin: 0;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+`;
+const Image = styled.img`
+width: 80%;
+  aspect-ratio: 1/1;
+  object-fit: contain;
+  margin: 1rem auto;
+`;
+const PriceTag = styled.p`
+background-color: ${(props) =>
+  product.category === 'electronics' ? 'red' : 'green'}};
+  border-radius: 2rem;
+  color: white;
+  font-weight: bold;
+  padding: 0.5rem;
+  text-align: center;
+  width: 100px;
+  margin: 1rem auto;
+  cursor: pointer;
+  border: 1px solid white;
+
+  &:hover{
+    border: 1px solid black;
+  color: black;
+  font-size: 0.75rem;
+  padding: 0.4rem;
+  }
+`;
+export default function Product(props) {
+  const { product } = props;
+
+  return (
+    <ProductCard>
+      <Category>{product.category}</Category>
+      <Image src={product.image} />
+      <PriceTag className={getClassName()}>{product.price}$</PriceTag>
+      <h3>{product.title}</h3>
+      <p className="description">{product.description}</p>
+    </ProductCard>
+  );
+}
